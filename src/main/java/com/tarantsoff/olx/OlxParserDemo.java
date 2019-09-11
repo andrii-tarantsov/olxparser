b835b612dbb1cfa87a6876753ee5cf4e46490056
Andrii Tarantsov <tarantsov.andrii@gmail.com>
2019-09-11 23:35:28 +0300

    removed redundant throws

--- src/main/java/com/tarantsoff/olx/OlxParserDemo.java
+++ src/main/java/com/tarantsoff/olx/OlxParserDemo.java
@@ -3,12 +3,11 @@ package com.tarantsoff.olx;
 import com.tarantsoff.Item;
 import org.jsoup.nodes.Document;
 
-import java.io.IOException;
 import java.util.List;
 
 public class OlxParserDemo {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Document doc = new OlxQuery().requestForOneBedrooms();
 
