864649ef16e38b7f807837dc778df0547d4a3080
Andrii Tarantsov <tarantsov.andrii@gmail.com>
2019-09-11 11:40:45 +0300

    ResultParser test initial commit

--- src/test/java/com/tarantsoff/ItemParserTest.java
+++ src/test/java/com/tarantsoff/ItemParserTest.java
@@ -2,20 +2,36 @@ package com.tarantsoff;
 
 import com.tarantsoff.olx.ItemParser;
 import com.tarantsoff.olx.OlxQuery;
-import com.tarantsoff.olx.ResultsPageParser;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ItemParserTest {
     @Test
     public void testParse(){
         Document doc = new OlxQuery().requestForOneBadrooms();
 
-        ResultsPageParser resultsPageParser = new ResultsPageParser(new ItemParser());
+        Elements results = doc.select(
+                "#offers_table > tbody > tr > td > div > table > tbody");
+        ItemParser itemParser = new ItemParser();
+        List<Item> items = results
+                .stream()
+                .map(itemParser::parse)
+                .collect(Collectors.toList());
+        for (Item item : items) {
+            Assertions.assertNotNull(item.getLocation());
+            Assertions.assertNotNull(item.getHref());
+            Assertions.assertNotNull(item.getPrice());
+            Assertions.assertNotNull(item.getTitle());
+            Assertions.assertNotNull(item.getThumbSrc());
 
-        List<Item> items = resultsPageParser.parse(doc);
+            Assertions.assertTrue(item.getDataId() != 0);
+        }
+
 
     }
 }
